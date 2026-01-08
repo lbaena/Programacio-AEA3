651b40d3f51c5808967ed7a4c0485305ff4c154a
Lluc Baena Llansola <llucbaenallansola@gmail.com>
2026-01-08 16:39:14 +0100

    Package 01-Colors

---
+++ 01-Colors/src/main/java/pkgColors/Colors.java
@@ -0,0 +1,23 @@
+package pkgColors;
+
+public class Colors {
+
+    public static final String RESET = "\u001B[0m";
+
+    // Colores de texto estándar
+    public static final String NEGRO = "\u001B[30m";
+    public static final String ROJO = "\u001B[31m";
+    public static final String VERDE = "\u001B[32m";
+    public static final String AMARILLO = "\u001B[33m";
+    public static final String AZUL = "\u001B[34m";
+    public static final String PURPURA = "\u001B[35m";
+    public static final String CIAN = "\u001B[36m";
+    public static final String BLANCO = "\u001B[37m";
+
+    // Colores de fondo (Background)
+    public static final String FONDO_ROJO = "\u001B[41m";
+    public static final String FONDO_VERDE = "\u001B[42m";
+    public static final String FONDO_AMARILLO = "\u001B[43m";
+    public static final String FONDO_AZUL = "\u001B[44m";
+
+}
